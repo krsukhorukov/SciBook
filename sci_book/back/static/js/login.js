73fd117934c06a8f80cb14468441620181163a53
Emil Nishanov <emil.nishanov@advncd.com>
2023-11-08 12:16:54 +0500

    login&logout

--- sci_book/back/static/js/login.js
+++ sci_book/back/static/js/login.js
@@ -1,8 +1,43 @@
-const switchers = [...document.querySelectorAll('.switcher')]
-
-switchers.forEach(item => {
-	item.addEventListener('click', function() {
-		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
-		this.parentElement.classList.add('is-active')
-	})
-})
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+	var $this = $(this),
+		label = $this.prev('label');
+  
+		if (e.type === 'keyup') {
+			  if ($this.val() === '') {
+			label.removeClass('active highlight');
+		  } else {
+			label.addClass('active highlight');
+		  }
+	  } else if (e.type === 'blur') {
+		  if( $this.val() === '' ) {
+			  label.removeClass('active highlight'); 
+			  } else {
+			  label.removeClass('highlight');   
+			  }   
+	  } else if (e.type === 'focus') {
+		
+		if( $this.val() === '' ) {
+			  label.removeClass('highlight'); 
+			  } 
+		else if( $this.val() !== '' ) {
+			  label.addClass('highlight');
+			  }
+	  }
+  
+  });
+  
+  $('.tab a').on('click', function (e) {
+	
+	e.preventDefault();
+	
+	$(this).parent().addClass('active');
+	$(this).parent().siblings().removeClass('active');
+	
+	target = $(this).attr('href');
+  
+	$('.tab-content > div').not(target).hide();
+	
+	$(target).fadeIn(600);
+	
+  });
--- sci_book/back/urls.py
+++ sci_book/back/urls.py
@@ -8,4 +8,5 @@ from django.conf.urls import handler500
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('', Main.as_view(), name='main'),
+    path('logout/', LogoutView.as_view(next_page='main'), name='logout'),
 ]
